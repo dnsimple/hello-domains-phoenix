3b8470a64def845d8ec963695928fd001b44252e
Javier Acero <j4cegu@gmail.com>
2017-01-05 10:10:01 +0100

    Move configuration to private functions

--- web/gateways/dnsimple.ex
+++ web/gateways/dnsimple.ex
@@ -1,17 +1,13 @@
 defmodule HelloDomains.Dnsimple do
 
-  @base_url Application.fetch_env!(:hello_domains, :dnsimple_client_base_url)
-  @client_id Application.fetch_env!(:hello_domains, :dnsimple_client_id)
-  @client_secret Application.fetch_env!(:hello_domains, :dnsimple_client_secret)
-
   # OAuth
 
   def authorize_url(options) do
-    Dnsimple.Oauth.authorize_url(client, @client_id, state: options[:state])
+    Dnsimple.Oauth.authorize_url(client, client_id, state: options[:state])
   end
 
   def exchange_authorization_for_token(attributes) do
-    attributes = Map.merge(attributes, %{client_id: @client_id, client_secret: @client_secret})
+    attributes = Map.merge(attributes, %{client_id: client_id, client_secret: client_secret})
     oauth_service.exchange_authorization_for_token(client, attributes)
   end
 
@@ -30,13 +26,13 @@ defmodule HelloDomains.Dnsimple do
   # Client
 
   def client do
-    %Dnsimple.Client{base_url: @base_url}
+    %Dnsimple.Client{base_url: base_url}
   end
   def client(%HelloDomains.Account{dnsimple_access_token: access_token}) do
     client(access_token)
   end
   def client(access_token) do
-    %Dnsimple.Client{base_url: @base_url, access_token: access_token}
+    %Dnsimple.Client{base_url: base_url, access_token: access_token}
   end
 
   # Service modules
@@ -52,4 +48,11 @@ defmodule HelloDomains.Dnsimple do
   defp domain_service do
     Application.get_env(:hello_domains, :dnsimple_domains_service, Dnsimple.Domains)
   end
+
+  # Configuration
+
+  def base_url,      do: Application.fetch_env!(:hello_domains, :dnsimple_client_base_url)
+  def client_id,     do: Application.fetch_env!(:hello_domains, :dnsimple_client_id)
+  def client_secret, do: Application.fetch_env!(:hello_domains, :dnsimple_client_secret)
+
 end
